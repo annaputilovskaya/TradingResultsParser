ccb5029c96b13b4f25077b78a537ed29312e9f35
anna putilovskaya <putilovskayaam@gmail.com>
2025-03-17 08:46:45 +0300

    fix generate_trading_result_objects function and rename file

---
+++ service_layer/results_generator.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from adapters.orm import ORMTradingResult
+from domain.model import TradingResult
+from service_layer.links_parser import get_date_from_link
+
+
+def generate_trading_result_objects(data: pd.DataFrame, link: str):
+    """Generates trading results objects from rows of DataFrame."""
+    for i in range(1, len(data) - 2):
+        row = data.iloc[i]
+        if (
+            pd.isna(row["Объем\nДоговоров\nв единицах\nизмерения"])
+            or row.apply(lambda x: "Итого" in str(x)).any()
+        ):
+            continue
+        else:
+            yield ORMTradingResult(
+                **TradingResult(
+                    exchange_product_id=row["Код\nИнструмента"],
+                    exchange_product_name=row["Наименование\nИнструмента"],
+                    delivery_basis_name=row["Базис\nпоставки"],
+                    volume=int(row["Объем\nДоговоров\nв единицах\nизмерения"]),
+                    total=int(row["Обьем\nДоговоров,\nруб."]),
+                    count=int(row["Количество\nДоговоров,\nшт."]),
+                    date=get_date_from_link(link),
+                ).to_dict()
+            )
